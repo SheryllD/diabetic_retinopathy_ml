dd00121442360dc6e1f3f50853a5b770eebd0c92
Sheryll Dumapal <sheryllhhs@outlook.com>
2025-07-17 21:21:20 +0200

    .h5

--- convert_model.py
+++ convert_model.py
@@ -1,14 +1,4 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.models import load_model
 
-model = Sequential([
-    Input(shape=(224, 224, 3), name="input_layer"),
-    Conv2D(32, (3,3), activation="relu"),
-    MaxPooling2D(2,2),
-    Flatten(),
-    Dense(2, activation="softmax")
-])
-
-# Save in .keras format for Streamlit compatibility
-model.save("CNN_model.keras")
-print("Saved model in 'CNN_model.keras' format.")
+model = load_model("CNN_model.keras", compile=False)
+model.save("CNN_model.h5")
